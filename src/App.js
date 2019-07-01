28ff8df2b933b66e292ccb1a130716af7b34918e
Allyn Alda <Teddy@om.home>
2019-07-01 12:18:15 +0200

    edited auth components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Landing from './components/auth/Landing'
+import SignUp from './components/auth/SignUp';
+import Login from './components/auth/Login';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Buho App Project</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>Buho App Project</h1>
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Route path='/signup' component={SignUp} />
+            <Route path='/login' component={Login} />
+          </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/auth/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Landing = () => {
+    return (
+        <div>
+        <h2>Landing Component</h2>
+            <Link to='/signup'> <button>Sign up</button></Link>
+            <Link to='/login'><button>Log in</button></Link>
+        </div>
+    )
+}
+
+export default Landing;
